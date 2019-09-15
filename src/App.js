36b7bab77cce319a6fde54dc6653c034fd00c6cf
Devyn Gish <devyngish@gmail.com>
2019-09-15 16:49:48 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, } from 'react-router-dom'
+import Home from "./components/Home"
+import Game from "./components/Game"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+ 
+ <>
+ 
+ <Switch>
+   <Route exact path= "/" component={Home} />
+   <Route exact path= "/game" component={Game} />
+   {/* <Route component={NoMatch}/> */}
+</Switch>
+</>
+
+)
+
 
 export default App;
